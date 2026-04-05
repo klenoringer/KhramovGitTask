8d98d607766166616c38d7e91a755a1c8d0fcfee
klenoringer <andy.04.ch@mail.ru>
2026-04-05 21:43:09 +0300

    Изменил код
    вы

---
+++ module_prerpocessing.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 16 09:35:16 2026
+
+@author: 2-106-03
+"""
+
+import sys
+
+try:
+    result = 1.00
+    for i in range(1, len(sys.argv)):
+        result *= float(sys.argv[i]) + float(sys.argv[i])
+except ValueError:
+    result = ''
+    for i in range(1, len(sys.argv)):
+        result += sys.argv[i]
+        
+print('Результат обработки:', result)
